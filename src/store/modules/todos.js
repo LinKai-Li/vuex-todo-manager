84551ba2e3c2b133e74eda6dffc33fe584e6e947
Link Li <nintendo.link@foxmail.com>
2024-12-28 01:44:53 +1100

    fetchTodos

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -8,7 +8,15 @@ const getters = {
   allTodos: (state) => state.todos,
 };
 
-const actions = {};
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos"
+    );
+
+    console.log(response.data);
+  },
+};
 
 const mutations = {};
 
